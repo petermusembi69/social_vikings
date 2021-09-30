aeed8cbafe44bac9489a3d3f4ff7338e4e127098
petermusembi69 <nyongesamusembi@gmail.com>
2021-09-30 20:18:25 +0300

    Remove unused code

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -11,39 +11,4 @@ class SignInDTO with _$SignInDTO {
       _$SignInDTOFromJson(json);
 }
 
-@freezed
-class AuthResult with _$AuthResult {
-  factory AuthResult(
-    AuthTokenResult data,
-    bool success,
-  ) = _AuthResult;
-
-  factory AuthResult.fromJson(Map<String, dynamic> json) =>
-      _$AuthResultFromJson(json);
-}
-
-@freezed
-class AuthTokenResult with _$AuthTokenResult {
-  factory AuthTokenResult(
-    @JsonKey(name: 'phone_number') String phoneNumber,
-    String token,
-    @JsonKey(name: 'token_type') String tokenType, {
-    @JsonKey(name: 'expires_in') int? expiresIn,
-  }) = _AuthTokenResult;
-
-  factory AuthTokenResult.fromJson(Map<String, dynamic> json) =>
-      _$AuthTokenResultFromJson(json);
-}
-
-@freezed
-class SignUpDTO with _$SignUpDTO {
-  factory SignUpDTO({
-    @JsonKey(name: 'country_id') required String countryId,
-    required String name,
-    @JsonKey(name: 'phone_number') required String phoneNumber,
-    required String password,
-  }) = _SignUpDTO;
-
-  factory SignUpDTO.fromJson(Map<String, dynamic> json) =>
-      _$SignUpDTOFromJson(json);
 }
