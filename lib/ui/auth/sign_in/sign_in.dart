9d1dd82a29cb2f4b5bf638cd0050221a7b30f19d
petermusembi69 <nyongesamusembi@gmail.com>
2021-10-11 17:30:33 +0300

    Change placeholder font

--- lib/ui/auth/sign_in/sign_in.dart
+++ lib/ui/auth/sign_in/sign_in.dart
@@ -156,7 +156,7 @@ class _SignInViewState extends State<SignInView> {
                             filled: true,
                             fillColor: Colors.white,
                             hintText: 'example@gmail.com',
-                            hintStyle: TextStyles.createShopPlaceHolderStyle,
+                            hintStyle: TextStyles.medium,
                             contentPadding: const EdgeInsets.symmetric(
                                 horizontal: 20, vertical: 5),
                           ),
@@ -199,7 +199,7 @@ class _SignInViewState extends State<SignInView> {
                             filled: true,
                             fillColor: Colors.white,
                             hintText: 'password',
-                            hintStyle: TextStyles.createShopPlaceHolderStyle,
+                            hintStyle: TextStyles.medium,
                             contentPadding: const EdgeInsets.symmetric(
                                 horizontal: 20, vertical: 5),
                           ),
@@ -339,15 +339,21 @@ class _SignInViewState extends State<SignInView> {
                                     style: TextStyles.subHeading,
                                   ),
                                 ]),
-                            loading: () => SizedBox(
-                              height: 20,
-                              width: 20,
-                              child: CircularProgressIndicator(
-                                strokeWidth: 2,
-                                backgroundColor:
-                                    AppTheme.appTheme(context).greyWeak,
-                              ),
-                            ),
+                            loading: () => Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Image.asset(
+                                    'images/google.png',
+                                    height: Adapt.px(32),
+                                  ),
+                                  SizedBox(
+                                    width: Adapt.px(20),
+                                  ),
+                                  Text(
+                                    'Google Sign In',
+                                    style: TextStyles.subHeading,
+                                  ),
+                                ]),
                             loaded: () => Row(
                                 mainAxisAlignment: MainAxisAlignment.center,
                                 children: [
