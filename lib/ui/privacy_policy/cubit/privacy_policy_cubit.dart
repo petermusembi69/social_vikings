27c7ff0c6d2a613605740c0456efa4e119f434cb
petermusembi69 <nyongesamusembi@gmail.com>
2021-10-11 16:32:03 +0300

    Add privacy policyy screen

--- lib/ui/privacy_policy/cubit/privacy_policy_cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'privacy_policy_state.dart';
-part 'privacy_policy_cubit.freezed.dart';
-
-class PrivacyPolicyCubit extends Cubit<PrivacyPolicyState> {
-  PrivacyPolicyCubit() : super(const PrivacyPolicyState.initial());
-}
--- lib/ui/privacy_policy/privacy_policy.dart
+++ lib/ui/privacy_policy/privacy_policy.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:soc/ui/widgets/drawer_wrapper.dart';
+import 'package:soc/utils/_index.dart';
 
 class PrivacyPolicy extends StatefulWidget {
   const PrivacyPolicy({Key? key}) : super(key: key);
@@ -7,9 +10,90 @@ class PrivacyPolicy extends StatefulWidget {
   _PrivacyPolicyState createState() => _PrivacyPolicyState();
 }
 
-class _PrivacyPolicyState extends State<PrivacyPolicy> {
+class _PrivacyPolicyState extends State<PrivacyPolicy>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+  }
+
+  _toggleAnimation() {
+    _animationController.isDismissed
+        ? _animationController.forward()
+        : _animationController.reverse();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return DrawerWraper(
+      animationController: _animationController,
+      page: Scaffold(
+        backgroundColor: Color(0xFFEFEFEF),
+        appBar: AppBar(
+          elevation: 0,
+          leadingWidth: Adapt.px(100),
+          backgroundColor: Color(0xFFEFEFEF),
+          centerTitle: true,
+          title: Text(
+            'Privacy Policy',
+            style: TextStyles.appBarTextStyle,
+          ),
+          leading: IconButton(
+            splashRadius: Adapt.px(10),
+            onPressed: () {
+              SystemChrome.setSystemUIOverlayStyle(
+                SystemUiOverlayStyle(
+                  statusBarColor: Colors.transparent,
+                  statusBarIconBrightness: _animationController.value == 0
+                      ? Brightness.light
+                      : Brightness.dark,
+                ),
+              );
+              _toggleAnimation();
+            },
+            icon: AnimatedIcon(
+              icon: AnimatedIcons.menu_close,
+              progress: _animationController,
+              color: AppTheme.appTheme(context).txt,
+            ),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: SizedBox(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                for (var i = 0; i < 5; i++)
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: Adapt.px(30),
+                      bottom: Adapt.px(30),
+                      left: Adapt.px(30),
+                      right: Adapt.px(10),
+                    ),
+                    child: Text(
+                      '''Veniam laboris est ad sunt dolore occaecat adipisicing excepteur sint voluptate magna id adipisicing. Dolore ea occaecat magna ullamco ullamco cillum culpa sit duis qui sunt sint aliqua proident. Consequat quis deserunt ex eiusmod ullamco ut aute consectetur eiusmod incididunt.
+Culpa et incididunt reprehenderit proident consequat.''',
+                      textAlign: TextAlign.justify,
+                      style: TextStyles.subHeading.copyWith(fontWeight: FontWeight.normal),
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
